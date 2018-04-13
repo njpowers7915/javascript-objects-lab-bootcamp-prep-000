7b4b394f0f1488de1703774a95ac56c00fe71ce8
Nicholas Powers <njpowers7915@gmail.com>
2018-04-13 12:38:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(obj, key) {
-  var newObj = Object.assign
+  var newObj = Object.assign({}, obj)
   delete newObj.key;
   return newObj;
 }
